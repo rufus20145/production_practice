491cf69094585fa3b657725b38affbbbe235e4ba
LAPTOP20145 <ivan20027749@gmail.com>
2023-07-06 18:10:28 +0300

    Обновлена программа для тестовых прогонов. Реализована обработка аргументов командной строки --dburl и --filename, а также обновлено API вслед за модулем мониторинга

--- call.py
+++ call.py
@@ -1,17 +1,37 @@
 """
 TODO module docstring
 """
-import logging
+import argparse
+import logging as log
 
 from monitor import ChangeMonitor
 
-logging.basicConfig(level=logging.INFO)
+DEFAULT_FILENAME = "connection_params.json"
 
-test = ChangeMonitor()
-init = test.get_initial_state()
-print(f"got inital data {init}")
+if __name__ == "__main__":
+    log.basicConfig(level=log.INFO)
 
-stroka = input("Нажмите Enter, чтобы продолжить")
+    parser = argparse.ArgumentParser()
 
-update = test.get_update()
-print(f"got update {update}")
+    parser.add_argument("--dburl", help="URL for the database in SQLAlchemy format")
+    parser.add_argument("--filename", help="Name of the file with database params")
+    args = parser.parse_args()
+
+    dburl = args.dburl
+    filename = args.filename
+
+    if dburl:
+        api = ChangeMonitor(dburl=dburl)
+    elif filename:
+        api = ChangeMonitor(filename=filename)
+    else:
+        log.warn("No dburl or filename specified. Using default.")
+        api = ChangeMonitor(filename=DEFAULT_FILENAME)
+
+    init_state = api.get_initial_state()
+    print(f"got inital data\n{init_state}")
+
+    input("Нажмите Enter, чтобы продолжить")
+
+    update = api.get_update()
+    print(f"got update data\n{update}")
