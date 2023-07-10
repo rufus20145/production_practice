4ae4934f8f12e4a621df68dda16ea48522605f3d
LAPTOP20145 <ivan20027749@gmail.com>
2023-07-10 10:54:17 +0300

    Реализована возможность чтения параметров подключения из файла JSON. Также изменён формат JSON-файла для соответствия требуемым SQLAlchemy параметрам

--- alchemy/base.py
+++ alchemy/base.py
@@ -1,3 +1,5 @@
+import json
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -5,9 +7,23 @@ Base = declarative_base()
 
 
 class Alchemy:
-    def __init__(self, dburl):
-        # TODO Добавить обработку, если передан файл с параметрами
-        self._dburl = dburl
+    def __init__(self, dburl=None, filename=None):
+        if dburl:
+            self._dburl = dburl
+        elif filename:
+            with open(filename, encoding="utf-8") as file:
+                data = json.load(file)
+
+                dialect = data["dialect"]
+                driver = data["driver"]
+                username = data["username"]
+                password = data["password"]
+                host = data["host"]
+                port = data["port"]
+                database = data["database"]
+
+                dburl = f"{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}"
+
         self._engine = create_engine(dburl)
         self._session_factory = sessionmaker(bind=self._engine)
 
